d5a726891a559e7dba9d483f6bb10cb22791aa3c
achien-k <achien-k@student.42porto.com>
2023-08-28 12:59:22 +0100

    cd V2

--- src/built-ins/cd.c
+++ src/built-ins/cd.c
@@ -6,36 +6,11 @@
 /*   By: achien-k <achien-k@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 12:05:42 by achien-k          #+#    #+#             */
-/*   Updated: 2023/08/28 10:51:33 by achien-k         ###   ########.fr       */
+/*   Updated: 2023/08/28 12:58:37 by achien-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-#include <stdbool.h>
-#include <string.h>
-#include <unistd.h>
-
-bool	equal_str(const char *s1, const char *s2)
-{
-	if (ft_strcmp(s1, s2) == 0)
-		return (true);
-	else
-		return (false);
-}
-
-char	*env_find_value(t_env *env, char *target)
-{
-	t_env	*tmp;
-	
-	tmp = env;
-	while (tmp)
-	{
-		if (equal_str(target, tmp->key))
-			return (tmp->value);
-		tmp = tmp->next;
-	}
-	return (NULL);
-}
 
 bool	env_change(t_shell *shell, char *target, char *new_value)
 {
@@ -44,10 +19,10 @@ bool	env_change(t_shell *shell, char *target, char *new_value)
 	tmp = shell->env;
 	while (tmp)
 	{
-		if (equal_str(target, tmp->key))
+		if (ft_strcmp(target, tmp->key) == 0)
 		{
 			free(tmp->value);
-			tmp->value = new_value;
+			tmp->value = ft_strdup(new_value);
 			return (true);
 		}
 		tmp = tmp->next;
@@ -58,22 +33,22 @@ bool	env_change(t_shell *shell, char *target, char *new_value)
 bool	ms_chdir(t_shell *shell, char *path)
 {
 	char	*tmp_pwd;
-	char	*tmp_path;
 
 	tmp_pwd = getcwd(NULL, 0);
-	tmp_path = ft_strdup(path);
 	if (chdir(path) != 0)
 	{
 		free(tmp_pwd);
-		free(tmp_path);
 		return (false);
 	}
 	env_change(shell, "OLDPWD", tmp_pwd);
-	env_change(shell, "PWD", tmp_path);
+	free(tmp_pwd);
+	tmp_pwd = getcwd(NULL, 0);
+	env_change(shell, "PWD", tmp_pwd);
+	free(tmp_pwd);
 	return (true);
 }
 
-void	path_slash(char *cdpath, char **path)
+char	*path_slash(char *cdpath, char **path)
 {
 	char	*tmp;
 
@@ -82,22 +57,20 @@ void	path_slash(char *cdpath, char **path)
 		if (*path[0] != '/')
 		{
 			tmp = ft_strjoin("/", *path);
-			free(*path);
-			*path = tmp;
+			return (tmp);
 		}
 		else 
-			return ;
+			return (ft_strdup(cdpath));
 	}
 	else 
 	{
 		if (*path[0] == '/')
 		{
 			tmp = ft_strdup(*path + 1);
-			free(*path);
-			*path = tmp;
+			return (tmp);
 		}
 		else 
-			return ;
+			return (ft_strdup(cdpath));
 	}
 }
 
@@ -105,6 +78,7 @@ bool	cdpath(t_shell *shell, char *path)
 {
 	char	**cdpath;
 	char	*tmp;
+	char	*tmp_path;
 	int		i;
 	
 	if (!get_env("CDPATH", shell) || path[0] == '/')
@@ -113,8 +87,8 @@ bool	cdpath(t_shell *shell, char *path)
 	i = 0;
 	while (cdpath[i])
 	{
-		path_slash(cdpath[i], &path);
-		tmp = ft_strjoin(cdpath[i++], path);
+		tmp_path = path_slash(cdpath[i++], &path);
+		tmp = ft_strjoin(tmp_path, path);
 		if (ms_chdir(shell, tmp))
 		{
 			ft_free_array(cdpath);
@@ -122,6 +96,7 @@ bool	cdpath(t_shell *shell, char *path)
 			return (true);
 		}
 		free(tmp);
+		free(tmp_path);
 	}
 	ft_free_array(cdpath);
 	return (false);
@@ -143,18 +118,41 @@ void	hyphen_cd_print(t_shell *shell, char *pwd)
 
 void	ms_cd(t_shell *shell, t_exec *cmd)
 {
-	char	*err;
-	
-	if (cmd->argv[2])
-		ft_putstr_fd("cd: too many arguments", STDERR_FILENO);
 	if (!cmd->argv[1])
 	{
-		if (!ms_chdir(shell, env_find_value(shell->env, "HOME")))
+		if (!ms_chdir(shell, get_env("HOME", shell)))
+			print_error(shell, "cd", "HOME variable", 2);
+	}
+	else 
+	{
+		if (cmd->argv[2])
+			print_error(shell, "cd", "too many arguments", 2);
+		else if (ft_strcmp(cmd->argv[1], "-") == 0)
+		{
+			if (!ms_chdir(shell, get_env("OLDPWD", shell)))
+				print_error(shell, "cd", "OLDPWD variable", 2);
+			hyphen_cd_print(shell, get_env("PWD", shell));
+		}
+		else if (!ms_chdir(shell, cmd->argv[1]) && !cdpath(shell, cmd->argv[1]))
+			print_error(shell, "cd: no such file or directory", cmd->argv[1], 2);
+	}
+	ft_putchar_fd('\n', STDOUT_FILENO);
+}
+
+/*static void	ms_cd_test(t_shell *shell, t_exec *cmd)
+{
+	char	*err;
+	
+	if (!cmd->argv[1])
+	{
+		if (!ms_chdir(shell, get_env("HOME", shell)))
 			ft_putstr_fd("cd: HOME variable", STDERR_FILENO);
 	}
 	else 
 	{
-		if (equal_str(cmd->argv[1], "-"))
+		if (cmd->argv[2])
+			ft_putstr_fd("cd: too many arguments", STDERR_FILENO);
+		else if (ft_strcmp(cmd->argv[1], "-") == 0)
 		{
 			if (!ms_chdir(shell, get_env("OLDPWD", shell)))
 				ft_putstr_fd("cd: OLDPWD variable", STDERR_FILENO);
@@ -168,37 +166,8 @@ void	ms_cd(t_shell *shell, t_exec *cmd)
 		}
 	}
 	ft_putchar_fd('\n', STDOUT_FILENO);
-	/*printf("New dir is %s\n", get_env("PWD", shell));
-	printf("OLDPWD is %s\n", get_env("OLDPWD", shell));*/
+	printf("Moved from\n%s\nto\n%s\n", get_env("OLDPWD", shell), get_env("PWD", shell));
 }
-
-/*void	ms_cd(t_shell *shell, t_exec *cmd)
-{
-	char	*err;
-	
-	if (cmd->argv[2])
-		print_error(shell, "cd: too many arguments", 2);
-	if (!cmd->argv[1])
-	{
-		if (!ms_chdir(shell, env_find_value(shell->env, "HOME")))
-			print_error(shell, "cd: HOME variable", 2);
-	}
-	else 
-	{
-		if (equal_str(cmd->argv[1], "-"))
-		{
-			if (!ms_chdir(shell, get_env("OLDPWD", shell)))
-				print_error(shell, "cd: OLDPWD variable", 2);
-			ft_putstr_fd(get_env("PWD", shell), STDOUT_FILENO);
-		}
-		else if (!ms_chdir(shell, cmd->argv[1]) && !cdpath(shell, cmd->argv[1]))
-		{
-			err = ft_strjoin("cd: no such file or directory: ", cmd->argv[1]);
-			print_error(shell, err, 2);
-			free(err);
-		}
-	}
-}*/
 
 static void	init_shell(t_shell *shell, char **envp)
 {
@@ -221,12 +190,16 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	init_shell(&shell, envp);
 	env_change(&shell, "CDPATH", cdpath);
-	cmd.argv[i] = "pwd";
+	cmd.argv[i] = ft_strdup("pwd");
 	while (argv[j])
-		cmd.argv[++i] = argv[j++];
+		cmd.argv[++i] = ft_strdup(argv[j++]);
 	cmd.argv[i + 1] = 0;
-	ms_cd(&shell, &cmd);
+	ms_cd_test(&shell, &cmd);
 	envp_destroy(shell.env);
+	i = 0;
+	while (cmd.argv[i])
+		free(cmd.argv[i++]);
+	ft_free_array(shell.envp);
 	free(cdpath);
 	return (0);
-}
+}*/

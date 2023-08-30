7656681cfb5ec9f198ee82625f1eded20d75c525
Luis Balsa <luisbalsa@gmail.com>
2023-08-30 12:21:36 +0100

    wait child in run_block
    wait child in run_block

--- src/run_cmd3.c
+++ src/run_cmd3.c
@@ -6,7 +6,7 @@
 /*   By: luide-so <luide-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 16:42:00 by luide-so          #+#    #+#             */
-/*   Updated: 2023/08/29 16:43:45 by luide-so         ###   ########.fr       */
+/*   Updated: 2023/08/30 12:20:54 by luide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,20 @@
 
 void	run_block(t_shell *shell, t_block *cmd)
 {
-	int		status;
-	int		pipefd[2];
 	pid_t	pid;
 
-	status = 0;
-	if (pipe(pipefd) == -1)
-		print_error(shell, "pipe error", NULL, 127);
 	pid = fork();
 	if (pid == -1)
 		print_error(shell, "fork error", NULL, 127);
 	if (pid == 0)
 	{
-		close(pipefd[0]);
 		run_cmd(shell, cmd->block);
+		wait_children(shell);
 		exit(g_exit);
 	}
-	else
-	{
-		waitpid(pid, &status, WUNTRACED);
-		if (WIFEXITED(status))
-			g_exit = WEXITSTATUS(status);
-		else if (WIFSIGNALED(status))
-			g_exit = WTERMSIG(status) + 128;
-	}
+	waitpid(pid, &g_exit, 0);
+	if (WIFEXITED(g_exit))
+		g_exit = WEXITSTATUS(g_exit);
+	else if (WIFSIGNALED(g_exit))
+		g_exit = WTERMSIG(g_exit) + 128;
 }

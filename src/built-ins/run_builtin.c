360b62317273d38dc92afb5982f82f368192054a
Luis Balsa <luisbalsa@gmail.com>
2023-09-06 01:47:24 +0100

    previous g_exit in exit
    previous g_exit in exit

--- src/built-ins/run_builtin.c
+++ src/built-ins/run_builtin.c
@@ -6,7 +6,7 @@
 /*   By: luide-so <luide-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 20:43:31 by luide-so          #+#    #+#             */
-/*   Updated: 2023/08/29 12:57:41 by achien-k         ###   ########.fr       */
+/*   Updated: 2023/09/06 01:43:06 by luide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	run_builtin(t_shell *shell, t_exec *cmd)
 {
 	(void)shell;
 
-	g_exit = 0;
+	if (ft_strcmp(cmd->argv[0], "exit"))
+		g_exit = 0;
 	if (!ft_strcmp(cmd->argv[0], "echo"))
 		ms_echo(cmd);
 	else if (!ft_strcmp(cmd->argv[0], "cd"))
@@ -30,5 +31,5 @@ void	run_builtin(t_shell *shell, t_exec *cmd)
 	else if (!ft_strcmp(cmd->argv[0], "env"))
 		ms_env(shell, cmd);
 	else if (!ft_strcmp(cmd->argv[0], "exit"))
-		ms_exit(shell, cmd); 
+		ms_exit(shell, cmd);
 }
